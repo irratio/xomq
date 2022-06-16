6d2173c6d99a8d15e0d27b5f957ac144cb57355c
Andrey Shmelkov <irratio@gmail.com>
2022-06-17 01:11:53 +0500

    classes! decomposition! options!

--- xomq.rb
+++ xomq.rb
@@ -1,93 +1,194 @@
 #!/usr/bin/env ruby
 
+require 'getoptlong'
 require 'ipaddr'
 require 'net/http'
 require 'openssl'
 require 'securerandom'
 require 'set'
 
-REPORT_REGEX = %r|(?<ip>\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}) - '(?<path>/[^']*)'|
+XomqConfig = Struct.new(:query_url, :secret_key, keyword_init: true) do
+  USAGE = <<~USAGE.freeze
+    Usage: #{$PROGRAM_NAME} -k <key> -u <url>
+    -k, --key   - secret key for access token generation,
+                  can also be set via XOMQ_SECRET_KEY env variable
+    -u, --url   - url for POST query,
+                  can also be set via XOMQ_QUERY_URL env variable
+    -h, --help  - this message
+  USAGE
 
-def query_url
-  @query_url ||= ENV['XOMQ_QUERY_URL']
-end
+  def process!
+    options = GetoptLong.new(
+      ['--key', '-k', GetoptLong::REQUIRED_ARGUMENT],
+      ['--url', '-u', GetoptLong::REQUIRED_ARGUMENT],
+      ['--help', '-h', GetoptLong::NO_ARGUMENT]
+    )
+    options.quiet = true
 
-def secret_key
-  @secret_key ||= ENV['XOMQ_SECRET_KEY']
-end
-
-def nonce
-  @nonce ||= SecureRandom.hex(6)
-end
-
-def request_body
-  %({"report_type":"plain","nonce":"#{nonce}"})
-end
-
-def access_token
-  @access_token ||= OpenSSL::HMAC.hexdigest('SHA256', secret_key, request_body)
-end
-
-def post_headers
-  {
-    'Content-Type': 'application/json',
-    'Authorization': access_token
-  }
-end
-
-def each_body_line(resp)
-  knob = ''
-  resp.read_body do |chunk|
-    *nuggets, new_knob = chunk.split("\n", -1)
-    if nuggets.empty?
-      knob << new_knob
-      next
+    options.each do |option, value|
+      case option
+      when  '--key'
+        self.secret_key ||= value
+      when '--url'
+        self.query_url ||= value
+      when '--help'
+        @need_help = true
+      end
     end
 
-    nuggets.first.prepend(knob)
-    nuggets.each { |nugget| yield nugget }
-    knob = new_knob
+    self.secret_key ||= ENV['XOMQ_SECRET_KEY']
+    self.query_url ||= ENV['XOMQ_QUERY_URL']
+
+    @problem = 'Please provide configuration.' unless filled?
+  rescue GetoptLong::MissingArgument, GetoptLong::InvalidOption => e
+    @problem = e.message
   end
-  yield knob unless knob.empty?
+
+  def usage
+    USAGE
+  end
+
+  def filled?
+    secret_key && query_url
+  end
+
+  def need_help?
+    @need_help
+  end
+
+  def problem
+    @problem
+  end
 end
 
+class XomqLoader
+  class HTTPError < StandardError; end
 
-uri = URI(query_url)
-use_ssl = uri.is_a?(URI::HTTPS)
+  def initialize(config:)
+    @config = config
+  end
 
-stats = Hash.new { |h, k| h[k] = Set.new }
+  def nonce
+    @nonce ||= SecureRandom.hex(6)
+  end
 
-begin
-  Net::HTTP.start(uri.host, uri.port, use_ssl: use_ssl) do |http|
-    request = Net::HTTP::Post.new(uri, post_headers)
-    request.body = request_body
+  def request_body
+    %({"report_type":"plain","nonce":"#{nonce}"})
+  end
 
-    http.request request do |response|
-      unless response.code == '200'
-        $stderr.puts "something happened:: #{response.message}"
-        puts response.inspect
-        exit 1
-      end
+  def access_token
+    @access_token ||= OpenSSL::HMAC.hexdigest('SHA256', secret_key, request_body)
+  end
 
-      each_body_line(response) do |line|
-        data = line.match(REPORT_REGEX)
+  def post_headers
+    {
+      'Content-Type': 'application/json',
+      'Authorization': access_token
+    }
+  end
 
-        unless data
-          $stderr.puts "bad string ignored: #{line.strip}"
-          next
+  def secret_key
+    @config.secret_key
+  end
+
+  def uri
+    @uri ||= URI(@config.query_url)
+  end
+
+  def use_ssl?
+    uri.is_a?(URI::HTTPS)
+  end
+
+  def reset!
+    @access_token = nil
+    @nonce = nil
+  end
+
+  def each_line
+    Net::HTTP.start(uri.host, uri.port, use_ssl: use_ssl?) do |http|
+      request = Net::HTTP::Post.new(uri, post_headers)
+      request.body = request_body
+
+      http.request(request) do |response|
+        raise HTTPError.new("#{response.code} #{response.message}") unless response.is_a?(Net::HTTPSuccess)
+
+        knob = ''
+        response.read_body do |chunk|
+          *nuggets, new_knob = chunk.split("\n", -1)
+          if nuggets.empty?
+            knob << new_knob
+            next
+          end
+
+          nuggets.first.prepend(knob)
+          nuggets.each { |nugget| yield nugget }
+          knob = new_knob
         end
 
-        path = data[:path]
-        ip = IPAddr.new(data[:ip]).to_i
-        stats[path] << ip
-      rescue IPAddr::InvalidAddressError
-        $stderr.puts "bad ip ignored: #{line.strip}"
+        yield knob unless knob.empty?
+      ensure
+        reset!
       end
     end
   end
-rescue => e
-  $stderr.puts "something happened: #{e}"
-  exit 1
 end
 
-stats.each { |path, ips| puts "#{path} - #{ips.size}" }
+class XomqParser
+  REPORT_REGEX = %r|(?<ip>\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}) - '(?<path>/[^']*)'|
+
+  def initialize(err_io: $stderr)
+    @stats = Hash.new { |h, k| h[k] = Set.new }
+    @err_io = err_io
+  end
+
+  def to_proc
+    lambda do |line|
+      data = line.match(REPORT_REGEX)
+
+      unless data
+        @err_io.puts "bad string ignored: #{line.strip}"
+        next
+      end
+
+      path = data[:path]
+      ip = IPAddr.new(data[:ip]).to_i
+      @stats[path] << ip
+    rescue IPAddr::InvalidAddressError
+      @err_io.puts "bad ip ignored: #{line.strip}"
+      next
+    end
+  end
+
+  def report
+    @stats.map { |path, ips| "#{path} - #{ips.size}" }
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  config = XomqConfig.new
+  config.process!
+
+  if config.problem
+    $stderr.puts config.problem
+    $stderr.puts
+    $stderr.puts config.usage
+    exit 1
+  end
+
+  if config.need_help?
+    $stderr.puts config.usage
+    exit
+  end
+
+  loader = XomqLoader.new(config: config)
+  parser = XomqParser.new
+
+  begin
+    loader.each_line(&parser)
+  rescue => e
+    $stderr.puts "error: #{e}"
+    exit 2
+  end
+
+  puts parser.report
+end
